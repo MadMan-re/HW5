2df39852f9b97905c96402c8bedf407dcd77c460
MadMan-re <madyan.agbaria@gmail.com>
2025-07-09 19:19:28 +0300

    wet part

--- enigma.py
+++ enigma.py
@@ -1,11 +1,51 @@
 
 class Enigma:
     def __init__(self, hash_map, wheels, reflector_map):
-        pass
+        self.hash_map = hash_map
+        self.wheels = wheels
+        self.reflector_map = reflector_map
 
     def encrypt(self, message):
-        pass
+        incryptedmessage = ""
+        for char in message:
+            i =  self.hash_map.get(char)
+            toAdd = ((self.wheels[0] * 2) - self.wheels[1] + self.wheels[2]) % 26 
+            if toAdd == 0:
+                i = i + 1
+            else:
+                i = i + toAdd
+            i = i % 26
+            for k in self.hash_map.keys():
+                if self.hash_map[k] == i:
+                    c1 = k
+            c2 = self.reflector_map[c1]
+            i = self.reflector_map[c2]
+            if toAdd == 0:
+                i = i - 1
+            else:
+                i = i - toAdd
+            i = i % 26
+            for j in self.hash_map.keys():
+                if self.hash_map[j] == i :
+                    c3 = j
+            incryptedmessage += c3
+        return incryptedmessage
 
+class JSONFileException(Exception):
 
-def load_enigma_from_path(path):
     pass
+ 
+def load_enigma_from_path(path):
+    import json
+    try:
+        with open(path,'r') as f:
+            data = json.load(f)
+        wheels = data["wheels"]
+        hash_map = data["hash_map"]
+        reflector_map = data["reflector_map"]
+    except Exception as e:
+        raise JSONFileException
+    enigma = Enigma(hash_map,wheels,reflector_map)
+    return enigma
+
+
